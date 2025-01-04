77db4bd23a03f2215c7419986478e228b3a7aca4
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi05.upvnet.upv.es>
2025-01-04 12:23:14 +0100

    Añadida implementación de la clase Point2D; Makefile actualizado

---
+++ Point2D.cpp
@@ -0,0 +1,27 @@
+#include "Point2D.h"
+
+// Constructor
+Point2D::Point2D(double x, double y) : x(x), y(y) {}
+
+// Método estático para calcular la distancia euclidiana entre dos puntos
+double Point2D::distance(const Point2D &a, const Point2D &b) {
+    return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2));
+}
+
+// Sobrecarga del operador ==
+bool operator==(const Point2D &a, const Point2D &b) {
+    return a.x == b.x && a.y == b.y;
+}
+
+// Sobrecarga del operador !=
+bool operator!=(const Point2D &a, const Point2D &b) {
+    return !(a == b);
+}
+
+// Sobrecarga del operador <<
+std::ostream& operator<<(std::ostream &out, const Point2D &p) {
+    out << "(" << p.x << ", " << p.y << ")";
+    return out;
+}
+
+
